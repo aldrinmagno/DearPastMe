2c4a840ac0f9433dfd0129aa71064ed4a98e58f2
Educ8 Admin <97067242+educ8-xp@users.noreply.github.com>
2023-05-01 02:06:55 +0930

    add favicon

--- pages/coming-soon/index.tsx
+++ pages/coming-soon/index.tsx
@@ -53,8 +53,8 @@ const ComingSoon = () => {
           content="DearPast.me is a platform that allows users to write letters to their past selves and share them with a community of people who are also looking to heal and reflect on their past. It is a safe and private space for self-discovery, reflection, gratitude, inspiration, and healing. It aims to provide users with a tool to process and understand their past experiences, find closure and move forward. It also allows users to read and gain inspiration from other's letters, which can help them with their journey of self-discovery and healing."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <link rel="icon" href="/images/favicon.png" />
+       </Head>
     <div className="coming-soon-area">
       <div className="d-table">
         <div className="d-table-cell">
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <title>DearPastMe: Write to Your Past, Heal Your Future</title>
         <meta name="description" content="DearPast.me is a platform that allows users to write letters to their past selves and share them with a community of people who are also looking to heal and reflect on their past. It is a safe and private space for self-discovery, reflection, gratitude, inspiration, and healing. It aims to provide users with a tool to process and understand their past experiences, find closure and move forward. It also allows users to read and gain inspiration from other's letters, which can help them with their journey of self-discovery and healing." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.png" />
       </Head>
       <Layout>
         
